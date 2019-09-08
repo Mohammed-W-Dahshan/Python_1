5213cf99a03f61ebb9d9c80110d706a1344c3e69
Mohammed Dahshan <mhamad.dahshan98@gmail.com>
2019-09-08 20:38:38 +0300

    add new std

--- FirstDB.py
+++ FirstDB.py
@@ -2,6 +2,7 @@ from db_operations import Students
 import sqlite3
 
 s1 = Students()
+s2 = Students()
 # res = s1.insert_std('Noor','Naser',21,'Noor@gmail.com')
 # rows = s1.get_all_std()
 # for i in rows:
