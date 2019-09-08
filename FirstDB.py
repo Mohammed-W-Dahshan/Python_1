0d056b0ab59aac38e91de222533aeb64e8f2966b
Mohammed Dahshan <mhamad.dahshan98@gmail.com>
2019-09-08 21:06:16 +0300

    test

--- FirstDB.py
+++ FirstDB.py
@@ -2,7 +2,7 @@ from db_operations import Students
 import sqlite3
 
 s1 = Students()
-# res = s1.insert_std('alla','alla',22,'alla@gmail.com')
+res = s1.insert_std('alla','alla',22,'alla@gmail.com')
 # rows = s1.get_all_std()
 # for i in rows:
 #     print(i)
